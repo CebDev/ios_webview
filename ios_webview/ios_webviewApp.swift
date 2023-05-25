79b0bd635db04f833f593c4e2b338ef3652dc9e3
Sébastien <sebvermandele@gmail.com>
2023-05-25 09:33:03 -0400

    Initial Commit

---
+++ ios_webview/ios_webviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_webviewApp.swift
+//  ios_webview
+//
+//  Created by Sébastien Vermandele on 2023-05-25.
+//
+
+import SwiftUI
+
+@main
+struct ios_webviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
